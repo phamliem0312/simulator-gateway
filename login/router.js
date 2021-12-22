eb766e9d7a06054a37df2ea965eb87392889c67f
liem pham <liem.phamthanh@ncc.asia>
2021-12-22 22:23:12 +0700

    create check time lock login

--- login/router.js
+++ login/router.js
@@ -7,7 +7,8 @@ const cfg = {
     userDb: "phamliem",
     passwordDb: "061102",
     databaseName: "my-project-nodejs",
-    tableName: "users"
+    tableName: "users",
+    time: 30
 }
 
 const conn = mysql.createConnection({
@@ -20,37 +21,54 @@ const conn = mysql.createConnection({
 router.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
+    const currentTime = new Date();
 
-    conn.query('SELECT * FROM `' + cfg.tableName + '` WHERE username = ?',[username], (err, results) => {
+    conn.query('SELECT * FROM `' + cfg.tableName + '` WHERE username = ?', [username], (err, results) => {
         if (err) {
             console.log(err);
             return res.status(404).send("Error occured while query db");
         }
-        console.log(results[0]);
         if (!results[0]) {
             return res.send("Username not exists");
         }
 
         if (results[0].status == 'L') {
-            return res.send("User is locked");
-        }
+            const timeL = results[0].timeL;
+            let diff = (currentTime.getTime() - timeL.getTime()) / 60000;
+            console.log(diff);
 
-        if (results[0].password != password) {
-            let loginF = parseInt(results[0].loginF + 1);
-            if (loginF == 5) {
-                conn.query('UPDATE `' + cfg.tableName + '` SET status= ? WHERE username = ?', ['L', username]);
-            }else{
-                conn.query('UPDATE `' + cfg.tableName + '` SET loginF= ? WHERE username = ?',[loginF, username]);
+            if (diff < cfg.time) {
+                return res.send("User is locked");
+            } else {
+                conn.query('UPDATE `' + cfg.tableName + '` SET loginF= ?, status= ?  WHERE username = ?', [0, 'A', username], (err, result => {
+                    if (results[0].password != password) {
+                        let loginF = 1;
+                        conn.query('UPDATE `' + cfg.tableName + '` SET loginF= ? WHERE username = ?', [loginF, username]);
+                        return res.send("Invalid password");
+                    }
+
+                    return res.send("Login success");
+                }));
             }
-            
-            return res.send("Invalid password");
+        } else {
+            if (results[0].password != password) {
+                let loginF = parseInt(results[0].loginF + 1);
+                if (loginF == 5) {
+                    const lockTime = new Date();
+                    conn.query('UPDATE `' + cfg.tableName + '` SET status= ?, timeL = ? WHERE username = ?', ['L', lockTime, username]);
+                    return res.send("Invalid password");
+                } else {
+                    conn.query('UPDATE `' + cfg.tableName + '` SET loginF= ? WHERE username = ?', [loginF, username]);
+                    return res.send("Invalid password");
+                }
+            }
+
+            conn.query('UPDATE `' + cfg.tableName + '` SET loginF= ?  WHERE username = ?', [0, username]);
+
+            res.send("Login success");
         }
-
-        conn.query('UPDATE `' + cfg.tableName + '` SET loginF= ?  WHERE username = ?',[0, username]);
-
-        res.send("Login success");
     });
-    
+
 });
 
 module.exports = router;
